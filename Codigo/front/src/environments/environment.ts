0c22bccb900b3f757f3c59569f90879346c4376d
Gustavo Alvarenga <arcgustavo10@gmail.com>
2026-09-21 23:47:31 -0300

    feat(deploy): prepara frontend para Container Apps

--- Codigo/front/src/environments/environment.ts
+++ Codigo/front/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '/api/v1'
+  apiBaseUrl: 'https://ca-dafabi-api-dev.yellowbay-d86b3364.mexicocentral.azurecontainerapps.io/api/v1'
 };
